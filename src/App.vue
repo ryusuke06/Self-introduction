569ceb9a345b89d88762e544ecd8220f8e343923
ryusuke06 <or.06.012@gmail.com>
2020-08-24 23:28:40 +0900

    vuetifyを用いた簡易自己紹介

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app dark>
+    <!--    Input my contests    -->
+    <v-content>
+      <Header />
+      <Top />
+      <Footer />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+import Top from "./components/Top";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// import Works from "./components/Works";
+// import TopPage from "./components/TopPage";
+
+export default {
+  name: "App",
+  components: {
+    // TopPage,
+    Header,
+    Top,
+    Footer,
+
+    // Works,
+  },
+  icons: {
+    iconfont: "mdi", // default - only for display purposes
+  },
+};
+</script>
---
+++ src/components/Top.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-content>
+    <section id="hero">
+      <v-row no-gutters>
+        <v-img
+          :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+          src="../assets/images/frame.jpg"
+        >
+          <v-theme-provider dark>
+            <v-container fill-height>
+              <v-row
+                align="center"
+                class="black--text mx-auto"
+                justify="center"
+              >
+                <v-col class="black--text text-center" cols="12" tag="h1">
+                  <span
+                    :class="[
+                      $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2',
+                    ]"
+                    class="font-weight-light"
+                  >
+                    WELCOME TO
+                  </span>
+
+                  <br />
+
+                  <span
+                    :class="[
+                      $vuetify.breakpoint.smAndDown ? 'display-3' : 'display-4',
+                    ]"
+                    class="font-weight-black"
+                  >
+                    MY PAGE
+                  </span>
+                </v-col>
+
+                <v-btn
+                  class="align-self-end"
+                  fab
+                  outlined
+                  @click="$vuetify.goTo('#about-me')"
+                >
+                  <v-icon>mdi-chevron-double-down</v-icon>
+                </v-btn>
+              </v-row>
+            </v-container>
+          </v-theme-provider>
+        </v-img>
+      </v-row>
+    </section>
+
+    <section id="about-me">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">ABOUT ME</h2>
+
+        <v-responsive class="mx-auto mb-8" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-responsive
+          class="mx-auto title font-weight-light mb-8"
+          max-width="720"
+        >
+          <p>楽しいこと大好きスケさんです！</p>
+          <p>何でも挑戦していく精神でやってます！</p>
+          <p>どうぞよろしくお願いします( ͡° ͜ʖ ͡°)</p>
+        </v-responsive>
+
+        <v-avatar class="elevation-12 mb-12" size="128">
+          <v-img src="../assets/images/mac.jpg"></v-img>
+        </v-avatar>
+      </v-container>
+    </section>
+  </v-content>
+</template>
